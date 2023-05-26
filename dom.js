035274f19863c98249d00a9aebfcffdda7763023
jai1998kishan <jaikishangupta754@gmail.com>
2023-05-26 15:59:55 +0530

    makinglistmorefun

--- dom.js
+++ dom.js
@@ -19,13 +19,34 @@
 //Get element by id;
 
 // console.log(document.getElementById('header-title'));
-var headerTitle=document.getElementById('header-title');
-var header=document.getElementById('main-header');
+// var headerTitle=document.getElementById('header-title');
+// var header=document.getElementById('main-header');
 // console.log(headerTitle);
 // headerTitle.textContent='hello';
 // headerTitle.innerText='goodbye';
 // headerTitle.innerHTML='<h3>Hello</h3>';
-header.style.borderBottom='solid 3px #000';
-var h2Head=document.getElementById('h1Text');
-h2Head.style.color='green';
-h2Head.style.fontWeight='bold';
+// header.style.borderBottom='solid 3px #000';
+// var h2Head=document.getElementById('h1Text');
+// h2Head.style.color='green';
+// h2Head.style.fontWeight='bold';
+
+//Get elements by className
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[1]);
+items[1].textContent='Hello 2';
+items[1].style.fontWeight='bold';
+items[1].style.backgroundColor='yellow';
+
+//this will give error because list are in array
+// items.style.backgroundColor='#f4f4f4';
+
+// for(var i=0;i<items.length;i++){
+//     items[i].style.backgroundColor='#f4f4f4';
+// }
+
+for(var i=0;i<items.length;i++){
+    items[i].style.fontWeight='bold';
+}
+
+items[2].style.backgroundColor='green';
