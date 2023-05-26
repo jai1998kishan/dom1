be90962fee508c00ce73ba76bb3b71b1b6403be7
jai1998kishan <jaikishangupta754@gmail.com>
2023-05-26 16:24:37 +0530

    getelementbyclassName

--- dom.js
+++ dom.js
@@ -30,23 +30,48 @@
 // h2Head.style.color='green';
 // h2Head.style.fontWeight='bold';
 
-//Get elements by className
-var items=document.getElementsByClassName('list-group-item');
-console.log(items);
-console.log(items[1]);
-items[1].textContent='Hello 2';
-items[1].style.fontWeight='bold';
-items[1].style.backgroundColor='yellow';
 
-//this will give error because list are in array
-// items.style.backgroundColor='#f4f4f4';
+//Get elements by className
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// console.log(items[1]);
+// items[1].textContent='Hello 2';
+// items[1].style.fontWeight='bold';
+// items[1].style.backgroundColor='yellow';
+
+// //this will give error because list are in array
+// // items.style.backgroundColor='#f4f4f4';
 
 // for(var i=0;i<items.length;i++){
 //     items[i].style.backgroundColor='#f4f4f4';
 // }
 
-for(var i=0;i<items.length;i++){
-    items[i].style.fontWeight='bold';
-}
+// for(var i=0;i<items.length;i++){
+//     items[i].style.fontWeight='bold';
+// }
 
-items[2].style.backgroundColor='green';
+// items[2].style.backgroundColor='green';
+
+
+//Get element by tagName:
+
+// var li=document.getElementsByTagName('li');
+// console.log(li);
+// console.log(li[1]);
+// li[1].textContent='Hello 2';
+// li[1].style.fontWeight='bold';
+// li[1].style.backgroundColor='yellow';
+
+// for(var i=0;i<li.length;i++){
+//         li[i].style.backgroundColor='#f4f4f4';
+//     }
+
+
+//query Selector;
+// var header = document.querySelector('#main-header');
+// header.style.borderBottom = 'solid 4px #ccc';
+
+// var input=document.querySelector('input');
+// input.value='Hello world!';
+
+
